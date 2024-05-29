5e16644cff664858de3db1e8fe8e102d4755de89
Hello-Github <pnsopgaming8097@gmail.com>
2024-05-29 21:32:32 +0530

    Added optional chaining operator to `authState.user.username` in HomePage.jsx.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ function HomePage() {
     <div className="flex justify-center items-center h-screen">
     <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-sm text-center">
       {authState.isAuthenticated ? (
-        <h2 className="text-2xl font-bold">Welcome, {authState.user.username}!</h2>
+        <h2 className="text-2xl font-bold">Welcome, {authState.user?.username}!</h2>
       ) : (
         <h2 className="text-2xl font-bold">Welcome to our website!</h2>
       )}
